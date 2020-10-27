134044e7ffbc535395296b04b63d8682fc0c97ea
xiaoll <2528055001@qq.com>
2020-10-27 17:28:12 +0800

    Merge branch 'dev' of C:\Users\Administrator\Desktop\SEHomework with conflicts.

--- src/main/java/com/example/test/controller/ProjectController.java
+++ src/main/java/com/example/test/controller/ProjectController.java
@@ -1,84 +1,43 @@
 package com.example.test.controller;
 
-import com.example.test.Jwt.JwtUtils;
 import com.example.test.bean.EmployeeBean;
+import com.example.test.bean.ProjectBean;
 import com.example.test.bean.SubTaskBean;
-import com.example.test.communication.addSubTaskBean;
-import com.example.test.service.LogService;
-import com.example.test.service.ProjectService;
-import com.example.test.util.ServiceUtil;
-import io.jsonwebtoken.Claims;
+import com.example.test.service.DataQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @CrossOrigin
 public class ProjectController {
-
     @Autowired
-    private ProjectService projectService;
-    @Autowired
-    private LogService logService;
+    DataQueryService dataQueryService;
 
-    @RequestMapping("/addSubTask")
+    @RequestMapping("/getRunProject")
     @ResponseBody
-    public JSONResult addSubTask(HttpServletRequest request, @RequestBody addSubTaskBean data) {
-        String userId = JwtUtils.analysis(request);
-        String result=projectService.addSubTask(data.getTaskBean(),data.getLeadingPath(),data.getSucceedingPath(),data.isChain());
-        if(result.contains(ServiceUtil.SUCCESS)){
-            String PId=data.getTaskBean().getSubTaskInProjectId();
-            logService.addLog(PId,userId,"新增了子任务"+PId);
-            return JSONResult.build(200,result,null);
-        }else {
-            System.out.println(result);
-            return JSONResult.build(500,result,null);
+    public JSONResult getRunProject(@RequestBody String empID) {
+        System.out.println("test");
+        System.out.println(empID);
+        List<ProjectBean> list = dataQueryService.getRunProject(empID);
+        if (list == null) {
+            return JSONResult.errorMessage("无此用户名");
+        } else {
+            return JSONResult.ok(list);
         }
     }
 
-    @RequestMapping("/deleteSubTask")
+    @RequestMapping("/getCompletedProject")
     @ResponseBody
-    public JSONResult deleteSubTask(HttpServletRequest request, @RequestBody String SubTaskID) {
-        String userId = JwtUtils.analysis(request);
-        String result=projectService.deleteSubTask(SubTaskID);
-        if(result.contains(ServiceUtil.SUCCESS)){
-            logService.addLog(SubTaskID,userId,"删除了子任务"+SubTaskID);
-            return JSONResult.build(200,result,null);
-        }else {
-            System.out.println(result);
-            return JSONResult.build(500,result,null);
+    public JSONResult getCompletedProject(@RequestBody String empID) {
+        System.out.println("test");
+        System.out.println(empID);
+        List<ProjectBean> list = dataQueryService.getCompletedProject(empID);
+        if (list == null) {
+            return JSONResult.errorMessage("无此用户名");
+        } else {
+            return JSONResult.ok(list);
         }
     }
-
-    @RequestMapping("/modifySubTask")
-    @ResponseBody
-    public JSONResult deleteSubTask(HttpServletRequest request, @RequestBody SubTaskBean subTaskBean) {
-        String userId = JwtUtils.analysis(request);
-        String result=projectService.modifySubTask(subTaskBean);
-        if(result.contains(ServiceUtil.SUCCESS)){
-            logService.addLog(subTaskBean.getSubTaskInProjectId(),userId,"修改了子任务"+subTaskBean.getSubTaskInProjectId());
-            return JSONResult.build(200,result,null);
-        }else {
-            System.out.println(result);
-            return JSONResult.build(500,result,null);
-        }
-    }
-
-    @RequestMapping("/forceCompleteSubTask")
-    @ResponseBody
-    public JSONResult forceCompleteSubTask(HttpServletRequest request, @RequestBody String SubTaskID) {
-        String userId = JwtUtils.analysis(request);
-        String result=projectService.forceCompleteSubTask(SubTaskID);
-        if(result.contains(ServiceUtil.SUCCESS)){
-            logService.addLog(SubTaskID,userId,"强制结束了任务"+SubTaskID);
-            return JSONResult.build(200,result,null);
-        }else {
-            System.out.println(result);
-            return JSONResult.build(500,result,null);
-        }
-    }
-
-
 }
--- src/main/java/com/example/test/controller/TaskController.java
+++ src/main/java/com/example/test/controller/TaskController.java
@@ -4,11 +4,83 @@ import com.example.test.bean.EmployeeBean;
 import com.example.test.bean.SubTaskBean;
 import com.example.test.service.DataQueryService;
 import com.example.test.service.LoginService;
+import com.example.test.service.SubTaskService;
 import com.example.test.service.TaskLogService;
+import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
+
+class ApplyTask {
+    String ApplicantID;
+    String HelpersID;
+    String subTaskId;
+
+    public String getApplicantID() {
+        return ApplicantID;
+    }
+
+    public void setApplicantID(String applicantID) {
+        ApplicantID = applicantID;
+    }
+
+    public String getHelpersID() {
+        return HelpersID;
+    }
+
+    public void setHelpersID(String helpersID) {
+        HelpersID = helpersID;
+    }
+
+    public String getSubTaskId() {
+        return subTaskId;
+    }
+
+    public void setSubTaskId(String subTaskId) {
+        this.subTaskId = subTaskId;
+    }
+}
+
+class ApplyTaskHandOver {
+    String ApplicantID;
+    String HelpersID;
+    String subTaskId;
+    Date taskOutSourceEndTime;
+
+    public String getApplicantID() {
+        return ApplicantID;
+    }
+
+    public void setApplicantID(String applicantID) {
+        ApplicantID = applicantID;
+    }
+
+    public String getHelpersID() {
+        return HelpersID;
+    }
+
+    public void setHelpersID(String helpersID) {
+        HelpersID = helpersID;
+    }
+
+    public String getSubTaskId() {
+        return subTaskId;
+    }
+
+    public void setSubTaskId(String subTaskId) {
+        this.subTaskId = subTaskId;
+    }
+
+    public Date getTaskOutSourceEndTime() {
+        return taskOutSourceEndTime;
+    }
+
+    public void setTaskOutSourceEndTime(Date taskOutSourceEndTime) {
+        this.taskOutSourceEndTime = taskOutSourceEndTime;
+    }
+}
 
 @RestController
 @CrossOrigin
@@ -17,6 +89,8 @@ public class TaskController {
     //将Service注入Web层
     @Autowired
     DataQueryService dataQueryService;
+    @Autowired
+    SubTaskService subTaskService;
 
     @RequestMapping("/getEmpDoingTask")
     @ResponseBody
@@ -41,6 +115,89 @@ public class TaskController {
             return JSONResult.errorMessage("无此用户名");
         } else {
             return JSONResult.ok(list);
+        }
+    }
+
+    @RequestMapping("/outSourcingApply")
+    @ResponseBody
+    public JSONResult outSourcingApply(@RequestBody ApplyTask applyTask) {
+        System.out.println("test");
+        System.out.println(applyTask.getApplicantID());
+        String msg = subTaskService.outSourcingApply(applyTask.ApplicantID, applyTask.getHelpersID(),
+                applyTask.getSubTaskId());
+        if (msg == null) {
+            return JSONResult.errorMessage("无此用户名");
+        } else {
+            return JSONResult.ok(msg);
+        }
+    }
+
+    @RequestMapping("/outSourcingHandover")
+    @ResponseBody
+    public JSONResult outSourcingHandover(@RequestBody ApplyTaskHandOver applyTaskHandOver) {
+        System.out.println("test");
+        System.out.println(applyTaskHandOver.getApplicantID());
+        String msg = subTaskService.outSourcingHandover(applyTaskHandOver.ApplicantID, applyTaskHandOver.getHelpersID(),
+                applyTaskHandOver.getSubTaskId(), applyTaskHandOver.taskOutSourceEndTime);
+        if (msg == null) {
+            return JSONResult.errorMessage("出现异常");
+        } else {
+            return JSONResult.ok(msg);
+        }
+        //TODO 发送通知：外包申请
+    }
+
+    @RequestMapping("/outSourcingRecovery")
+    @ResponseBody
+    public JSONResult outSourcingRecovery(@RequestBody ApplyTask applyTask) {
+        System.out.println("test");
+        System.out.println(applyTask.getApplicantID());
+        String msg = subTaskService.outSourcingRecovery(applyTask.ApplicantID, applyTask.getHelpersID(),
+                applyTask.getSubTaskId());
+        if (msg == null) {
+            return JSONResult.errorMessage("出现异常");
+        } else {
+            return JSONResult.ok(msg);
+        }
+    }
+
+    @RequestMapping("/subTaskCompleteApply")
+    @ResponseBody
+    public JSONResult subTaskCompleteApply(@RequestBody SubTaskBean subTaskBean) {
+        System.out.println("test");
+        System.out.println(subTaskBean.getSubTaskInProjectId());
+        String msg = subTaskService.subTaskCompleteApply(subTaskBean.getSubTaskId());
+        if (msg == null) {
+            return JSONResult.errorMessage("出现异常");
+        } else {
+            return JSONResult.ok(msg);
+        }
+    }
+
+    @RequestMapping("/subTaskCompleteConclusion")
+    @ResponseBody
+    public JSONResult subTaskCompleteConclusion(@RequestBody SubTaskBean subTaskBean) {
+        System.out.println("test");
+        System.out.println(subTaskBean.getSubTaskInProjectId());
+        String msg = subTaskService.subTaskCompleteConclusion(subTaskBean.getSubTaskId());
+        if (msg == null) {
+            return JSONResult.errorMessage("出现异常");
+        } else {
+            return JSONResult.ok(msg);
+        }
+    }
+
+
+    @RequestMapping("/subTaskCompleteRejection")
+    @ResponseBody
+    public JSONResult subTaskCompleteRejection(@RequestBody SubTaskBean subTaskBean) {
+        System.out.println("test");
+        System.out.println(subTaskBean.getSubTaskInProjectId());
+        String msg = subTaskService.subTaskCompleteConclusion(subTaskBean.getSubTaskId());
+        if (msg == null) {
+            return JSONResult.errorMessage("出现异常");
+        } else {
+            return JSONResult.ok(msg);
         }
     }
 }
